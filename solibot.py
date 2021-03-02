4e13a8e588a39cd4676885cec4913c8beff0d1f3
Jinu Augustine <jkarock@hotmail.com>
2021-03-02 11:36:17 +0530

    Update solibot.py

--- solibot.py
+++ solibot.py
@@ -6,7 +6,6 @@ from functools import reduce
 from similarity import find_most_similar
 import requests, json
 import pymysql
-from google.cloud import translate
 
 
 faqdb = pymysql.connect(user='faquser', passwd='Faq@123',
@@ -42,16 +41,31 @@ WEATHER_INPUTS = ["how's the weather", "how is the weather", "how is the weather
 
 
 # Generating response
-def response(user_response, raw_response, detected_lang, category):
-    query = user_response
-    answer = find_most_similar(category, query)
-    if (answer['score'] > confidence_score['min_score']):
-        # set off event asking if the response question is what they were looking for
-        print ("\nBest-fit question: %s (Score: %s)\nAnswer: %s\n" % (answer['question'],
-                                                                        answer['score'],
-                                                                        answer['answer']))
-        SoliBot_response = [answer['answer'], answer['score']]
+def response(user_response, raw_response, conj_response, detected_lang, category):
+    query1 = user_response
+    answer1 = find_most_similar(category, query1)
 
+    query2 = conj_response
+    answer2 = find_most_similar(category, query2)
+
+    if (answer1['score'] > confidence_score['min_score']):
+        # set off event asking if the response question is what they were looking for
+        print ("\nBest-fit question with Keywords: %s (Score: %s)\nAnswer: %s\n" % (answer1['question'],
+                                                                        answer1['score']*100,
+                                                                        answer1['answer']))
+
+    if (answer2['score'] > confidence_score['min_score']):
+        # set off event asking if the response question is what they were looking for
+        print ("\nBest-fit question with Full Query: %s (Score: %s)\nAnswer: %s\n" % (answer2['question'],
+                                                                        answer2['score']*100,
+                                                                        answer2['answer']))
+
+    if (answer1['score']>answer2['score']):
+        print("Selected Question: ",answer1['question'])
+        SoliBot_response = [answer1['answer'], answer1['image'], answer1['video']]
+    elif(answer1['score']<answer2['score']):
+        print("Selected Question: ",answer2['question'])
+        SoliBot_response = [answer2['answer'], answer2['image'], answer2['video']]
     else:
         try:
             #Sending Un-Answered Query to Database
@@ -59,11 +73,11 @@ def response(user_response, raw_response, detected_lang, category):
             val = (raw_response, detected_lang, user_response)
             cursor.execute(sql, val)
             faqdb.commit()
-            print(cursor.rowcount, "Un-Answered Question pushed to FAQ Database")
+            print("Couldn't find an answer :(\nUn-Answered Question pushed to FAQ Database")
             SoliBot_response = "I'm sorry i didn't catch that! \nCould you please rephrase that query? \n\nI will learn from my experts and I will be able to answer you next time."
         except:
             SoliBot_response = "I'm sorry i didn't catch that! \nCould you please rephrase that query? \n\nI will learn from my experts and I will be able to answer you next time."
-            print("Un-Answered couldn't be pushed due to server error!")
+            print("Un-Answered couldn't be pushed due to Server Error!")
     return SoliBot_response
 
 
@@ -82,30 +96,6 @@ def weather_data(latitude, longitude):
         # except:
         #     weather_result = "Sorry I'm not able to fetch the Weather details currently. \nPlease try again later..."
     return weather_result
-
-def translate_text(text="What is Tea", project_id="chatbot-translation-306311"):
-
-    client = translate.TranslationServiceClient()
-
-    location = "global"
-
-    parent = f"projects/{project_id}/locations/{location}"
-
-    # Detail on supported types can be found here:
-    # https://cloud.google.com/translate/docs/supported-formats
-    response = client.translate_text(
-        request={
-            "parent": parent,
-            "contents": [text],
-            "mime_type": "text/plain",  # mime types: text/plain, text/html
-            "source_language_code": "en-US",
-            "target_language_code": "fr",
-        }
-    )
-
-    # Display the translation for each input text provided
-    for translation in response.translations:
-        print("Translated text: {}".format(translation.translated_text))
 
 @app.route('/', methods = ['GET']) 
 def home(): 
@@ -133,6 +123,7 @@ def query_handler():
 
 
     raw_response=raw_response.lower()
+    conj_response = raw_response
     try:
         comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
         result_det = json.dumps(comprehend.detect_dominant_language(Text = raw_response), sort_keys=True, indent=4)
@@ -176,30 +167,18 @@ def query_handler():
         final_img = ""
         final_vid = ""
     else:
-        resp = response(user_response, raw_response, detected_lang, category)
-        try:
-            sql = "SELECT CONVERT(a_image USING utf8) FROM user_qa WHERE answer = %s;"
-            val = (resp)
-            cursor.execute(sql, (val,))
-            a_img = cursor.fetchone()
-            for x in a_img:
-                final_img = x
+        resp = response(user_response, raw_response, conj_response, detected_lang, category)
+        f_resp = resp[0]
+        final_img = resp[1]
+        final_vid = resp[2]
 
-            sql = "SELECT a_link FROM user_qa WHERE answer = %s;"
-            val = (resp)
-            cursor.execute(sql, (val,))
-            a_vid = cursor.fetchone()
-            for x in a_vid:
-                final_vid = x
-        except:
-            final_img = ""
-            final_vid = ""
-    
     try:
-        trans2 = translate.translate_text(Text=resp, SourceLanguageCode="en", TargetLanguageCode=detected_lang)
+        trans2 = translate.translate_text(Text=f_resp, SourceLanguageCode="en", TargetLanguageCode=detected_lang)
         final_response = trans2["TranslatedText"]
     except:
         final_response = resp
+
+    faqdb.close()
 
     server_response = {'response': final_response,
                     'image': final_img,
