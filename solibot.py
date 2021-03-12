57cd15ab33b61eb09d5d721dc60d1fcec39f86c6
Jinu Augustine <jkarock3@gmail.com>
2021-03-12 15:05:24 +0530

    final

--- solibot.py
+++ solibot.py
@@ -117,12 +117,9 @@ def translation(text, target):
     if isinstance(text, six.binary_type):
         text = text.decode("utf-8")
 
-    # Text can also be a sequence of strings, in which case this method
-    # will return a sequence of results for each text.
     result = translate_client.translate(text, target_language=target)
 
     translated_text = result["translatedText"]
-    print(u"Detected source language: {}".format(result["detectedSourceLanguage"]))
 
     return translated_text
 
