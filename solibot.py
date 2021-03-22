fc0ade9e0ccdead40ae64bb1330d0877d2dfb6de
Jinu Augustine <jkarock3@gmail.com>
2021-03-22 15:45:19 +0530

    final

--- solibot.py
+++ solibot.py
@@ -399,15 +399,15 @@ def query_handler():
     except:
         final_response = f_resp
 
-    try:
-        farmer_final = input_data['farmer_name']
-        with faqdb.connection.cursor() as cursor:
-                    sql = "INSERT INTO output_logs (device_id, user_name, qa_date, query_category, query_native, language, query_translated, bot_response) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-                    val = (device_id, farmer_final, today, category, raw_response, detected_lang, trans_response, final_response)
-                    cursor.execute(sql, val)
-                    faqdb.connection.commit()
-    except:
-        pass
+    # try:
+    farmer_final = input_data['farmer_name']
+    with faqdb.connection.cursor() as cursor:
+                sql = "INSERT INTO output_logs (device_id, user_name, qa_date, query_category, query_native, language, query_translated, bot_response) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+                val = (device_id, farmer_final, today, category, raw_response, detected_lang, trans_response, final_response)
+                cursor.execute(sql, val)
+                faqdb.connection.commit()
+    # except:
+    #     pass
 
 
     server_response = {'response': final_response,
